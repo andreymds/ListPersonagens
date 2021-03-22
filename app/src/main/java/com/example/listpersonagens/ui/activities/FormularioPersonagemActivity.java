251fc10fe0f5ef120f46bf7cb1b933732c45dfc3
andreymds <andrey_mds@hotmail.com>
2021-03-22 01:55:32 -0300

    Vínculo dos objetos do XML com os códigos em JAVA

--- app/src/main/java/com/example/listpersonagens/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listpersonagens/ui/activities/FormularioPersonagemActivity.java
@@ -3,8 +3,13 @@ package com.example.listpersonagens.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.listpersonagens.R;
+import com.example.listpersonagens.model.Personagem;
 
 public class FormularioPersonagemActivity extends AppCompatActivity {
 
@@ -12,5 +17,37 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_personagem);
+
+        /*Vinculando os objetos do xml com as variáveis*/
+        EditText campoNome = findViewById(R.id.edittext_nome);
+        EditText campoAltura = findViewById(R.id.edittext_altura);
+        EditText campoNascimento = findViewById(R.id.edittext_nascimento);
+        Button botaoSalvar = findViewById(R.id.button_salvar);
+
+        /*Instanciando uma View*/
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+
+            /*Sobrescrevendo Método de Click*/
+            @Override
+            public void onClick(View v) {
+
+                /*Pega informação das caixas de texto e armazena os valores nas variáveis*/
+                String nome = campoNome.getText().toString();
+                String altura = campoAltura.getText().toString();
+                String nascimento = campoNascimento.getText().toString();
+
+                Personagem personagemSalvo = new Personagem(nome,altura,nascimento);
+
+                Toast.makeText(FormularioPersonagemActivity.this,
+                        personagemSalvo.getNome() + " - " + personagemSalvo.getAltura() + " - "
+                        + personagemSalvo.getNascimento(),
+                        Toast.LENGTH_SHORT).show();
+
+                /*Criação da Classe Personagem com os parâmetros nome, altura e nascimento*/
+                new Personagem(nome, altura, nascimento);
+
+
+            }
+        });
     }
 }
